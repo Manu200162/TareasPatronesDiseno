20d19137ffbcaeab5ac256e198ad36d1cb746d5b
Manu200162 <manli200134@gmail.com>
2021-06-12 18:58:40 -0400

    ejercicio 4

---
+++ src/main/java/TareaeEjercicios2doParcial/Strategy_4/BusquedaPequenia.java
@@ -0,0 +1,17 @@
+package TareaeEjercicios2doParcial.Strategy_4;
+
+import java.util.List;
+
+public class BusquedaPequenia implements IModoBusqueda{
+    @Override
+    public void realizarBusqueda(List<Celular> celularList, String filtroBusqueda) {
+        if(filtroBusqueda.contains("precio: ") && filtroBusqueda.contains("modelo: ")){
+            System.out.println("Se utlizara una busqueda comun para una busca por modelo y precio para la siguiente consulta:");
+        }else if(filtroBusqueda.contains("modelo: ")){
+            System.out.println("Se utlizara una busqueda comun para una busca por modelo para la siguiente consulta:");
+        }else if(filtroBusqueda.contains("precio: ")){
+            System.out.println("Se utlizara una busqueda comun para una busca por precio para la siguiente consulta:");
+        }
+        System.out.println(filtroBusqueda);
+    }
+}
